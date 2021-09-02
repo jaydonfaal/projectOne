46a1f7059e0dc06743bfbc0f12ea61207fe61b1d
andrewreiter4 <reiterandrew04@gmail.com>
2021-09-02 10:32:27 -0700

    Updated
    tests, works

--- bank.java
+++ bank.java
@@ -6,50 +6,51 @@ public class bank
 
 	public void deposit(String id, Double amnt)
 	{
-			if (amnt > 0)
+		if (amnt > 0)
+		{
+			if(map != null && map.containsKey(id))
 			{
-				if(map != null && map.containsKey(id))
-				{
-					map.put(id, map.get(id)+amnt);
-					System.out.println("added");
-				}
-				System.out.println("map does not contain key");
-				map.put(id, amnt);
+				map.put(id, map.get(id)+amnt);
 				System.out.println("added");
 			}
-			System.out.println("Enter Valid Amount");
+			System.out.println("Map does not contain key");
+			map.put(id, amnt);
+			System.out.println("added");
+		}
+		System.out.println("Enter Valid Amount");
 	}
 
 	public void withdraw (String id, double amnt)
 	{
-		
-			if (amnt > 0)
-			{
-				if (map != null && map.containsKey(id))
-				{
-					map.replace(id, map.get(id)- amnt);
-				}
-				else
-				{
-					System.out.println ("Account does not exist");
-				}
-			}
-		}
 
-	public String checkBalance (String id)
-	{
-		System.out.println(map.containsKey(id));
-		
-		if (map != null && map.containsKey(id))
+		if (amnt > map.get(id))
 		{
-			String ret =  "" + (map.get(id));
-			return ret;
+			System.out.println("Error: Amount invalid.");
+		}	
+		else if (map.get(id) == null)
+		{
+			System.out.println ("Account does not exist");
 		}
 		else
 		{
-			return ("Account does not exist");
+			map.replace(id, map.get(id)- amnt);
+		}
+
+	}
+
+public String checkBalance (String id)
+{
+	{
+		if (map.get(id) == null)
+		{
+			return("ID does not exist.");
+		}
+		else
+		{
+			return ""+map.get(id);
 		}
 	}
+}
 
 }
 
