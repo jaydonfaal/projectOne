794b76b9f6b56281bec8fb068a97bf1ad7461998
andrewreiter4 <reiterandrew04@gmail.com>
2021-09-02 09:59:16 -0700

    Update
    not testing correctly

--- bank.java
+++ bank.java
@@ -2,27 +2,60 @@ import java.util.HashMap;
 
 public class bank
 {
-  private HashMap<String, Double> map;
-  public bank()
-  {
-  HashMap<String, Double>  map = new HashMap<>();
-  }
+	private HashMap<String, Double> map = new HashMap<String, Double>();
+	private String id;
+	public bank(String id)
+	{
+		this.id = id;
+	}
 
-  public void deposit(String id, double amnt)
-  {
-    try
-    {
-      if(map.containsKey(id) == true)
-      {
-        map.replace(id, map.get(id)+amnt);
-      }
-      else
-      {
-        map.put(id, amnt);
-      }
-      }catch(NullPointerException e){}
+	public void deposit(String id, Double amnt)
+	{
+		try
+		{
+			if (amnt > 0)
+			{
+				if(map.containsKey(id))
+				{
+					map.put(id, map.get(id)+amnt);
+				}
+			}
+			else
+			{
+				map.put(id, amnt);
+			}
+		}catch(NullPointerException e){}
+	}
 
+	public void withdraw (String id, double amnt)
+	{
+		try
+		{
+			if (amnt > 0)
+			{
+				if (map.containsKey(id))
+				{
+					map.replace(id, map.get(id)- amnt);
+				}
+				else
+				{
+					System.out.println ("Account does not exist");
+				}
+			}
+		}catch(NullPointerException e){}}
 
-  }
+	public String checkBalance (String id)
+	{
+		if (map != null && map.containsKey(id))
+		{
+			String ret =  "" + (map.get(id));
+			return ret;
+		}
+		else
+		{
+			return ("Account does not exist");
+		}
+	}
 
 }
+
